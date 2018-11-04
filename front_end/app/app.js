e4341c04f76ddc6ba6f3c18bec5917cae9a320b9
Bard <polleyb@iupui.edu>
2018-11-04 17:54:40 -0500

    Ember initial build with comment templates

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -1,3 +1,16 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/app.js
+ * Author:
+ * Creation Date: 01 Nov 2018
+ * Feature: [Feature number and title]
+ * Purpose: Application load script
+ * Modification History:
+ * - 01 Nov 2018 (bwp) - Initial build (no features)
+ *
+ * Licensing Information
+ ***********************************************/
+
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
