17942b6425b233df3f7972179c8e773ea35c40ec
Bard <polleyb@iupui.edu>
2018-11-05 15:52:06 -0500

    Created basic route for login, with login page information required.  Will need to determine auto-redirection required

---
+++ front_end/app/routes/login.js
@@ -0,0 +1,16 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/routes/login.js
+ * Author:
+ * Creation Date: 05 Nov 2018
+ * Feature: 22 -login route
+ * Purpose: Login route / pash for application
+ * Modification History:
+ * - 05 Nov 2018 (bwp) - Initial build
+ * Licensing Information
+ ***********************************************/
+
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
