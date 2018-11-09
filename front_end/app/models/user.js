9b1f09a3e137853d2ff7e0b054f3c3031d622f2f
Bard <polleyb@iupui.edu>
2018-11-09 06:22:22 -0500

    Baseline User Model created for front-end and adapter use

--- front_end/app/models/user.js
+++ front_end/app/models/user.js
@@ -1,5 +1,20 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/models/user.js
+ * Author: Bard Polley
+ * Creation Date: 08 Nov 2018
+ * Feature: 21 -User Data Model
+ * Purpose: Login route / path for application
+ * Modification History:
+ * - 08 Nov 2018 (bwp) - Initial build
+ * Licensing Information
+ ***********************************************/
+
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  userName: DS.attr('string'),
+  passWord: DS.attr('string')
+
 
 });
