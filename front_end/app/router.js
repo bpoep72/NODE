edbe60e24897183363ecd3994488520e757bcd1b
Bard <polleyb@iupui.edu>
2018-11-09 21:38:28 -0500

    Added front-end onboarding route -- need to define data model and integrate with back-end

--- front_end/app/router.js
+++ front_end/app/router.js
@@ -5,10 +5,12 @@
  * Creation Date: 01 Nov 2018
  * Feature: Baseline
  *  22 -login route
+ *  46 - onboarding route
  * Purpose: Free-flow text with gist of filename
  * Modification History:
  * - 01 Nov 2018 (bwp) - Initial build (no features)
  * - 05 Nov 2018 (bwp) - Added route for login
+ * - 09 Nov 2018 (bwp) - Added route for onboarding
  * Licensing Information
  ***********************************************/
 
@@ -22,6 +24,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('login');
+  this.route('onboarding');
 });
 
 export default Router;
