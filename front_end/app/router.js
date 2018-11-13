1cde6fed7f0c390fc93387ae77fe993087ec0650
Bardley Polley <polleyb@iupui.edu>
2018-11-13 18:23:56 -0500

    Issue 42 - Added survey question on diet

--- front_end/app/router.js
+++ front_end/app/router.js
@@ -40,6 +40,7 @@ Router.map(function() {
         this.route('education');
         this.route('payment');
         this.route('smoking');
+        this.route('diet');
       });
     });
     this.route('usertype');
