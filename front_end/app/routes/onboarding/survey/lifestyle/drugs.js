bdb95058832cf33089e877bb9540089cf2d60e8e
Bardley Polley <polleyb@iupui.edu>
2018-11-13 06:08:36 -0500

    Survey question added for drugs

---
+++ front_end/app/routes/onboarding/survey/lifestyle/drugs.js
@@ -0,0 +1,15 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/routes/onboarding/survey/lifestyle/drugs.js
+ * Author: Bard Polley
+ * Creation Date: 13 Nov 2018
+ * Feature: 40 - Survey Drugs
+ * Purpose: Survey Question Behavior for Drugs
+ * Modification History:
+ * - 13 Nov 2018 (bwp) - Initial build
+ ***********************************************/
+
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
