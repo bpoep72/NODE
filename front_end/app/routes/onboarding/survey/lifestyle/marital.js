17ee759bf60b8933ebfaae917d0c42f7c3d1e7fd
Bardley Polley <polleyb@iupui.edu>
2018-11-14 11:17:23 -0500

    issue 41 - Survey (Marital Status) route / template built

---
+++ front_end/app/routes/onboarding/survey/lifestyle/marital.js
@@ -0,0 +1,15 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/routes/onboarding/survey/lifestyle/marital.js
+ * Author: Bard Polley
+ * Creation Date: 14 Nov 2018
+ * Feature: 41 -Survey Marital Status
+ * Purpose: Survey Question Behavior for Marital Status
+ * Modification History:
+ * - 14 Nov 2018 (bwp) - Initial build
+ ***********************************************/
+
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
