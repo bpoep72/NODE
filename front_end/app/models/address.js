836c41f819af3a14614617788ceccd11fbe7fba9
Bardley Polley <polleyb@iupui.edu>
2018-11-17 13:10:07 -0500

    issue 81 - Data model for address created

--- front_end/app/models/address.js
+++ front_end/app/models/address.js
@@ -3,8 +3,8 @@
  * File:  app/models/profile.js
  * Author: Bard Polley
  * Creation Date: 17 Nov 2018
- * Feature: 79 -User Profile Data Model
- * Purpose: Data Model for profile-specific object
+ * Feature: 81 -User Address Data Model
+ * Purpose: Data Model for address-specific object
  * Modification History:
  * - 17 Nov 2018 (bwp) - Initial build
  * Licensing Information
@@ -12,8 +12,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
-  fname: DS.attr('string'),
-  lname: DS.attr('string'),
-  user: DS.belongsTo('user', {async: true}
+  city: DS.attr('string'),
+  state: DS.attr('string'),
+  home: DS.attr('string'),
+  user: DS.belongsTo('user', {async: true})
 });
