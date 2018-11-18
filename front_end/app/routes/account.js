3c21f51044be2ead289b96ad5ab0b6833263552f
Bardley Polley <polleyb@iupui.edu>
2018-11-18 12:26:24 -0500

    Issue 89 - Account Route and GUI page built with placeholders

---
+++ front_end/app/routes/account.js
@@ -0,0 +1,15 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/routes/account.js
+ * Author: Bard Polley
+ * Creation Date: 18 Nov 2018
+ * Feature: 86 - Account route
+ * Purpose: Behavior for account route
+ * Modification History:
+ * - 18 Nov 2018 (bwp) - Initial build
+ ***********************************************/
+
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
