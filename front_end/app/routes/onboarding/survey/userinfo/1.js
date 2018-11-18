eb2fde1f32d24ab093f0e31edde653ad304f8a4b
aibearde <aibearde@iupui.edu>
2018-11-17 22:40:25 -0500

    Added a controller to 'PUT' user info from page 1 of onboarding

--- front_end/app/routes/onboarding/survey/userinfo/1.js
+++ front_end/app/routes/onboarding/survey/userinfo/1.js
@@ -7,8 +7,21 @@
  * Purpose: Behavior for user info page 1
  * Modification History:
  * - 11 Nov 2018 (bwp) - Initial build
+ * - 17 Nov 2018 (aib) - Adding 'put' to route to send data to backend so that a user account will be created
  ***********************************************/
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+
+// when button is submitted do a 'put' request to server
+
 
 export default Route.extend({
+    ajax: inject(),
+
+    model() {
+
+        
+
+    }
+
 });
