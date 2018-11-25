dcc19c5cfae115b7136d4a4438ecf28ab19ebe24
Bardley Polley <polleyb@iupui.edu>
2018-11-25 14:41:27 -0500

    Added authentication to each protected page to redirect to the login if not currently signed-in

--- front_end/app/routes/core/accounts.js
+++ front_end/app/routes/core/accounts.js
@@ -1,12 +1,13 @@
 /************************************************
  * Application:  NOD
- * File:  app/routes/account.js
+ * File:  app/routes/core/account.js
  * Author: Bard Polley
  * Creation Date: 18 Nov 2018
  * Feature: 86 - Account route
  * Purpose: Behavior for account route
  * Modification History:
  * - 18 Nov 2018 (bwp) - Initial build
+ * - 25 Nov 2018 (bwp) - Relocated under core
  ***********************************************/
 
 import Route from '@ember/routing/route';
