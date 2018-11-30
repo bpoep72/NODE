4b85948d27cd9df9fd3166594b77c41ed355da21
Bryce <bmpoeppe@iu.edu>
2018-11-30 01:57:04 -0500

    forgot about git

--- back-end/app/models/post.js
+++ back-end/app/models/post.js
@@ -3,8 +3,9 @@ const {Schema} = mongodb;
 const {Types: {referesTo}} = Schema;
 
 const schema = new Schema ({
-    dateMade: {type: String, required: true },
+    dateMade: {type: String, required: true, default: Date.now },
     content: {type: String, required: true },
+    owner: {type: String, required: true },
 });
 
 module.exports = mongodb.resource ('post', schema);
--- back-end/app/seeds/mongodb/$default.js
+++ back-end/app/seeds/mongodb/$default.js
@@ -1,6 +1,11 @@
 const { Seed } = require ('@onehilltech/blueprint-mongodb');
 const dab = require ('@onehilltech/dab');
 
+var number_of_accounts = 5;
+var number_of_posts = 80; //will randomly assign owners
+
+var maritalStatuses = ['single', 'married', 'divorced'];
+
 /**
  * @class
  */
@@ -16,20 +21,24 @@ module.exports = Seed.extend ({
             scope: ['gatekeeper.account.create'],
         }],
         accounts:
-        [{
-            username: 'default',
-            password: 'default',
-            email: 'default@no-replay.com',
-            scope: ['gatekeeper.account.create'],
-            enabled: true,
-        }],
+          dab.times(number_of_accounts, function(i) {
+              return {
+                 username: `user${i}`,
+                 password: `user${i}`,
+                 email: `user${i}@email.com`,
+                 scope: ['gatekeeper.account.create'],
+                 enabled: true,
+              };
+            }),
         user_tokens:
-        [{
-            client: dab.ref('native.0'),
-            account: dab.ref('accounts.0'),
-            refresh_token: dab.id(),
-            scope: ['gatekeeper.account.create'],
-        }],
+            dab.times(number_of_accounts, function() {
+              return {
+                client: dab.ref('native.0'),
+                account: dab.ref(dab.sample(dab.get('accounts'))),
+                refresh_token: dab.id(),
+                scope: ['gatekeeper.account.create'],
+              };
+            }),
         recaptcha:
         [{
             name: 'recaptcha',
@@ -43,6 +52,45 @@ module.exports = Seed.extend ({
             client: dab.ref ('native.0'),
             scope: ['gatekeeper.account.create'],
         }],
+        post:
+          dab.times(number_of_posts, function(i) {
+            return {
+              content: `test post #:${i}`,
+              owner: dab.ref(dab.sample(dab.get('accounts'))),
+            };
+          }),
+        profile:
+          dab.map(dab.get('accounts'), function (account, i) {
+              return {
+                owner: account._id,
+                fname: `John${i}`,
+                lname: `Doe${i}`,
+                age: Math.floor(Math.random() * 115),
+                ethnicity: `ethnicity${i}`,
+                orientation: `orientation${i}`,
+                height: Math.floor(Math.random() * 250) + "cm",
+                weight: Math.floor(Math.random() * 115) + "kg",
+              };
+          }),
+        survey:
+          dab.map(dab.get('accounts'), function (account) { //map maps each account to 1 entry
+            return {
+              owner: account._id,
+              drinker: !!Math.round(Math.random()), //!! means convert to boolean
+              smoker: !!Math.round(Math.random()),  //i know js is weird
+              recreationalDrugs: !!Math.round(Math.random()),
+              healthyDiet: Math.floor(Math.random() * 5),
+              maritalStatus: maritalStatuses[Math.floor(Math.random() * maritalStatuses.length)], //pick one at random
+              children: Math.floor(Math.random() * 90),
+            };
+          }),
+        //directDeposit:
+        //  dab.map(dab.get('accounts'), function(account) {
+        //    return {
+        //      owner: account._id,
+        //      
+        //    };
+        //  }),
     };
   }
 });
