bd2e61ee357fbe3e2949b015974275e69adb21b5
Bardley Polley <polleyb@iupui.edu>
2018-12-01 16:00:47 -0500

    issue-099 Profile Component

---
+++ front_end/app/components/user-profile.js
@@ -0,0 +1,14 @@
+/* Application:  NOD
+* File:  app/components/user-profile.js
+* Author: Bard Polley
+* Creation Date: 01 Dec 2018
+* Feature: 99 - Component view for User Profile
+* Purpose: Component for use in profile GUI
+* Modification History:
+  * - 01 Dec 2018 (bwp) - Initial build
+***********************************************/
+
+import Component from '@ember/component';
+
+export default Component.extend({
+});
