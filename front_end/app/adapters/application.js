bf03c771237c757730da465d3c44e73120b035da
Bardley Polley <polleyb@iupui.edu>
2018-12-02 11:54:31 -0500

    Initial push for profile/id testing

--- front_end/app/adapters/application.js
+++ front_end/app/adapters/application.js
@@ -1,3 +1,13 @@
+/************************************************
+ * Application:  NOD
+ * File:  app/adapters/application.js
+ * Author: Ember
+ * Creation Date: 18 Nov 2018
+ * Feature: XXX - Adapter specifications for Gatekeeper
+ * Purpose: Behavior for epecifying data format and translation for front-back integration
+ * Modification History:
+ * - 18 Nov 2018 (bwp) - Initial build
+ ***********************************************/
 import DS from 'ember-data';
 import RESTAdapter from 'ember-cli-gatekeeper/-lib/user/adapters/rest';
 
