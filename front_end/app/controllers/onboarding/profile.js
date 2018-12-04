b7e143042ccbc58c301ba59a15ae34a72f2196f3
Peter Daum <pddaum@iupui.edu>
2018-12-04 18:43:30 -0500

    POSTing profile after createRecord from onboarding/profile, back-end 400

--- front_end/app/controllers/onboarding/profile.js
+++ front_end/app/controllers/onboarding/profile.js
@@ -1,10 +1,30 @@
 import Controller from '@ember/controller';
+import Authenticated from 'ember-cli-gatekeeper/mixins/authenticated';
 
-export default Controller.extend({
+export default Controller.extend( Authenticated, {
   
   actions: {
     continue() {
-      // TO DO
+      // check if user filled out all fields?
+      console.log("First Name: ", this.fname);
+
+      // create the profile record and persist to database
+      let currentUser = this.get('currentUser');
+
+      console.log("Current User ID: ", currentUser.id);
+
+      this.store.createRecord( 'profile', {
+        id: currentUser.id,
+        fname: this.fname,
+        lname: this.lname,
+        age: this.age,
+        ethnicity: this.ethnicity,
+        orientation: this.orientation,
+        height: this.height,
+        weight: this.weight,
+      }).save();
+
+      // next page
       this.transitionToRoute('onboarding.lifestyle')
     }
   }
