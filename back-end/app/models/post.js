85416d63f131e377e2c7d38b56b55fd0914029d0
Bryce <bmpoeppe@iu.edu>
2018-12-04 11:46:07 -0500

    changed some models

--- back-end/app/models/post.js
+++ back-end/app/models/post.js
@@ -3,8 +3,16 @@ const {Schema} = mongodb;
 const {Types: {referesTo}} = Schema;
 
 const schema = new Schema ({
-    dateMade: {type: String, required: true, default: Date.now },
-    content: {type: String, required: true },
+    dateMade: {type: Date, required: true, default: Date.now},
+    desciption: {type: String, required: true, trim: true },
+    owner: { type: String, required: true, trim: true },
+    title: {type: String, required: true, trim: true },
+    comments: {type: String, trim: true },
+    rating: {type: Number },
+    endDate: {type: Date, required: true, },
+    payType: {type: String, trim: true },
+    payRate: {type: String, trim: true }
+    
 });
 
 module.exports = mongodb.resource ('post', schema);
