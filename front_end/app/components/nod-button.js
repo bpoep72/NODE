2e749721c09278cad1f9d614a636a4d6b361d28d
Peter Daum <pddaum@iupui.edu>
2018-12-04 17:37:26 -0500

    profile forms added for styling

--- front_end/app/components/nod-button.js
+++ front_end/app/components/nod-button.js
@@ -3,7 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   classNameBindings: ['notBlue:nod-button-pink:nod-button-blue'],
   notBlue: true,
-  title: 'Button',
+  title: '',
 
   click() {
     this.submit()
