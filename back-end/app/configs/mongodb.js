7cdc56143fb7d9ba34bfc11dc7ae96bc19eec93a
Bryce <bmpoeppe@iu.edu>
2018-12-04 12:12:01 -0500

    switched back to default collection

--- back-end/app/configs/mongodb.js
+++ back-end/app/configs/mongodb.js
@@ -1,7 +1,7 @@
 module.exports = {
     connections: {
         $default: {
-            uri: 'mongodb+srv://group4node:group4Node!@cluster0-aogrt.mongodb.net/test2?retryWrites=true',
+            uri: 'mongodb+srv://group4node:group4Node!@cluster0-aogrt.mongodb.net/test?retryWrites=true',
             seed: true,
             options: {useNewUrlParser: true}
         },
