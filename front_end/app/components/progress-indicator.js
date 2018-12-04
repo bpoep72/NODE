b604ea38251872056f08c23c2c1e6f84f98f27f5
Peter Daum <pddaum@iupui.edu>
2018-12-04 02:31:14 -0500

    Hiding progress-indicator in onboarding index now

--- front_end/app/components/progress-indicator.js
+++ front_end/app/components/progress-indicator.js
@@ -1,6 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['progress-indicator'],
+  classNameBindings: ['isHidden:progress-indicator-hidden:progress-indicator'],
+  isHidden: false,
   activePage: 1,
+
+  didRender() {
+//   this._super(...arguments);  // necessary if overriding init()
+    this.set('isHidden', this.activePage === 0)
+  },
+       
+  didUpdateAttrs(){
+    this.set('isHidden', this.activePage === 0)
+  }
 });
--- front_end/app/controllers/onboarding.js
+++ front_end/app/controllers/onboarding.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  activePage: 1,
+  activePage: 0,
     // will probably start activePage: 0 and hide progress-bar in this case
 });
