44677fc8fad9a88ee52e00ec1b0a7bd1bdb08d2a
Bryce <bmpoeppe@iu.edu>
2018-12-06 15:48:58 -0500

    endpoint documentation improvement

--- back-end/app/routers/endpoint.js
+++ back-end/app/routers/endpoint.js
@@ -1,14 +1,20 @@
 
-/*
- *This file generates the core access points for items
- *accessable via the blueprint.js server side application.
- *Cors is utilized to enable requests across domains and
- *implemented using the Node.js Express.js library.
- *Additionally the site handles authentication of users
- *via the blueprint-gatekeeper.js library extension also
- *developed by @onehilltech github and is mounted on the
- * /gatekeeper directory.
- */
+/************************************************
+ * Application:  address.js
+ * File:  app/models/address
+ * Author: Bryce Poeppel
+ * Purpose:
+ *    This file generates the core access points for items
+ *  accessable via the blueprint.js server side application.
+ *  Cors is utilized to enable requests across domains and
+ *  implemented using the Node.js Express.js library.
+ *  Additionally the site handles authentication of users
+ *  via the blueprint-gatekeeper.js library extension also
+ *  developed by @onehilltech github and is mounted on the
+ *  /gatekeeper directory.
+ *
+ * Licensing Information
+ ***********************************************/
 
 const blueprint = require ('@onehilltech/blueprint');
 const { Router } = blueprint;
@@ -25,7 +31,7 @@ const corsOptions = {
 module.exports = Router.extend ({
     specification :
     {
-        '/': //cors to enable use across all sub domains
+        '/': //cors at root to require use of cors across all sub domains
         {
             use: [cors(corsOptions)],
         },
