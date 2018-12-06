cfa89f79350cfcad23ad017d3228f8255f4698ee
Bryce <bmpoeppe@iu.edu>
2018-12-06 14:29:00 -0500

    BankName got lost in the sea of commits. This commit is to amend this issue.

--- back-end/app/models/directDeposit.js
+++ back-end/app/models/directDeposit.js
@@ -5,8 +5,9 @@ const {Types: {referesTo}} = Schema;
 const schema = new Schema ({
 	CCV: {type: String, required:true},
 	cardNumber: {type: String, required:true},
-    issuer: {type: String, required: true},
-    routingNumber: {type: String, required: true},
+  issuer: {type: String, required: true},
+  routingNumber: {type: String, required: true},
+	bankName: {type: String, required: true},
 });
 
 module.exports = mongodb.resource ('directDeposit', schema);
