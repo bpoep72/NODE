29dd8688cf34d784914ab7beadeb3aac2ed6496c
Bryce <bmpoeppe@iu.edu>
2018-12-06 15:46:17 -0500

    profile documentation

--- back-end/app/models/profile.js
+++ back-end/app/models/profile.js
@@ -1,3 +1,12 @@
+/************************************************
+ * Application:  profile.js
+ * File:  app/models/profile.js
+ * Author: Jose
+ * Purpose: To define the profile schema
+ *
+ * Licensing Information
+ ***********************************************/
+
 const mongodb = require ('@onehilltech/blueprint-mongodb');
 const {Schema} = mongodb;
 const {Types: {refersTo}} = Schema;
@@ -9,7 +18,7 @@ const schema = new Schema ({
 	ethnicity: {type: String, required:true},
 	orientation: {type: String, required:false},
 	height: {type: String, required:false},
-    weight: {type: String, required:false},
+  weight: {type: String, required:false},
 });
 
 module.exports = mongodb.resource ('profile', schema);
