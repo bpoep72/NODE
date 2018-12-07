93bd6cbe560f9886bdb362bd044c4ae83ba603f7
Jose Francisco <junior.poster@gmail.com>
2018-12-06 20:41:57 -0500

    Added description headers to post controller file and survey controller file

--- back-end/app/controllers/surveyController.js
+++ back-end/app/controllers/surveyController.js
@@ -1,3 +1,15 @@
+/************************************************
+ * Application:  NoDE
+ * File:  backend/app/controllers/surveyController.js
+ * Author: 
+ * Creation Date: 2018-11-14
+ * Purpose: Controller for the users' onboarding survey
+ * Related files: ../models/survey.js
+                  ../routers/endpoint.js
+ *
+ * Licensing Information
+ ***********************************************/
+
 const { model } = require ('@onehilltech/blueprint');
 const { ResourceController } = require ('@onehilltech/blueprint-mongodb');
 
